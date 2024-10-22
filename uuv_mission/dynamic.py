2418691f12d4320e5a641bb4cb6bc8a18263c8fb
Meilin Hixson <meilin.hixson@new.ox.ac.uk>
2024-10-22 20:54:47 +0000

    Modified mission class to include a new method to load a mission from a CSV file

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -76,7 +76,11 @@ class Mission:
     @classmethod
     def from_csv(cls, file_name: str):
         # You are required to implement this method
-        pass
+        extract = pd.read_csv(file_name)
+        reference = extract['reference'].values
+        cave_height = extract['cave_height'].values
+        cave_depth = extract['cave_depth'].values
+        return cls(reference, cave_height, cave_depth)   
 
 
 class ClosedLoop:
