a09eb703147d1f3cd06d9d06b82b6c9f67b8dc0c
Meilin Hixson <meilin.hixson@new.ox.ac.uk>
2024-10-22 21:42:55 +0000

    Modified PD controller for variable dt

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -7,8 +7,9 @@ class PDController:
         self.KD = KD  # Derivative gain
         self.previous_error = 0     # Error at the previous time step
 
-    def compute_control_action(self, reference, output):  
+    def compute_control_action(self, reference, output, dt):  
         error = reference - output
-        control_action = self.KP * error + self.KD * (error - self.previous_error)  
+        derivative = (error - self.previous_error) / dt
+        control_action = self.KP * error + self.KD * derivative  
         self.previous_error = error
         return control_action
