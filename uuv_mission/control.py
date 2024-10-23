4c15f3e49ae58dc8bc042a2a7fedac5c6e93405f
Meilin Hixson <meilin.hixson@new.ox.ac.uk>
2024-10-23 14:45:27 +0000

    Fixed capitalisation issues

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -2,14 +2,14 @@
 
 # control.py
 class PDController:
-    def __init__(self, KP=0.15, KD=0.6):  # Default gains
-        self.KP = KP # Proportional gain
-        self.KD = KD  # Derivative gain
+    def __init__(self, kp=0.15, kd=0.6):  # Default gains
+        self.kp = kp # Proportional gain
+        self.kd = kd  # Derivative gain
         self.previous_error = 0     # Error at the previous time step
 
     def compute_control_action(self, reference, output, dt):  
         error = reference - output
         derivative = (error - self.previous_error) / dt
-        control_action = self.KP * error + self.KD * derivative  
+        control_action = self.kp * error + self.kd * derivative  
         self.previous_error = error
         return control_action
